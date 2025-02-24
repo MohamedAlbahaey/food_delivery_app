0bb8a62674073d410d4ae8835291befac665a473
Mohamed Albahaey <144071699+MohamedAlbahaey@users.noreply.github.com>
2025-02-24 21:19:05 +0200

    feat: added the register page

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery_app/components/my_button.dart';
+import 'package:food_delivery_app/components/my_textfield.dart';
+
+class RegisterPage extends StatefulWidget {
+  final void Function()? onTap;
+
+  const RegisterPage({super.key, required this.onTap});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.lock_open,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height: 25),
+            
+            //message app slogan
+            Text(
+              "Let's create an account for you",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            //email input
+            MyTextField(
+              controller: emailController,
+              hintText: "Email", 
+              obsecureText: false,
+            ),
+
+            const SizedBox(height: 10),
+
+            //password input
+            MyTextField(
+              controller: passwordController,
+              hintText: "Password",
+              obsecureText: true,
+            ),
+
+            const SizedBox(height: 10),
+
+            //confirm password input
+            MyTextField(
+              controller: confirmPasswordController,
+              hintText: "Confirm password",
+              obsecureText: true,
+            ),
+
+            const SizedBox(height: 25),
+
+            //Sign up button
+            MyButton(
+              onTap: () {
+                print("Email: ${emailController.text}");
+                print("Password: ${passwordController.text}");
+              },
+              text: "Sign Up",
+            ),
+            
+            const SizedBox(height: 25),
+
+            //register button
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Already a member?",
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+                const SizedBox(width: 4),
+                GestureDetector(
+                  onTap: widget.onTap,
+                  child: Text(
+                    "Login Now",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            )
+
+          ],
+        ),
+      ),
+    );
+  }  
+}
