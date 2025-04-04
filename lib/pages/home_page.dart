0b0204177fa1ea99b3fa2767fe4beb49ecf5ef3e
Mohamed Albahaey <144071699+MohamedAlbahaey@users.noreply.github.com>
2025-04-04 21:50:48 +0200

    refactor: remove unnecessary import

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/components/my_current_location.dart';
 import 'package:food_delivery_app/components/my_description_box.dart';
